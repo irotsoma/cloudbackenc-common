50357c129b8e96d99570978a9c33dc3522d1ca98
irotsoma <irotsomadev@gmail.com>
2016-12-06 17:01:53 -0800

    Work on file controllers and build issues.

---
+++ common/src/main/kotlin/com/irotsoma/cloudbackenc/common/FileMetadata.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by irotsoma on 12/6/16.
+ */
+
+package com.irotsoma.cloudbackenc.common
+
+import java.util.*
+
+data class FileMetadata(
+        val senderID: UUID,
+        val senderFileID: UUID
+)

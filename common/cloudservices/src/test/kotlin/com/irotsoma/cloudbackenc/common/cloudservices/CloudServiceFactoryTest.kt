d089e9a963dae92cc79bae211abf7518a57fe9f5
irotsoma <irotsomadev@gmail.com>
2018-01-09 14:02:10 -0800

    fixed test

--- common/cloudservices/src/test/kotlin/com/irotsoma/cloudbackenc/common/cloudservices/CloudServiceFactoryTest.kt
+++ common/cloudservices/src/test/kotlin/com/irotsoma/cloudbackenc/common/cloudservices/CloudServiceFactoryTest.kt
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-package com.irotsoma.cloudbackenc.common.cloudservices;
+package com.irotsoma.cloudbackenc.common.cloudservices
 
 
 import org.junit.Test
